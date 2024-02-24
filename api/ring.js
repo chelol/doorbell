0e7951833301d3667d83990fdc7d4c4445b0dd88
Chelo <marcelo.tissoni@gmail.com>
2024-02-24 10:27:43 -0300

    Update ring.js

--- api/ring.js
+++ api/ring.js
@@ -16,7 +16,7 @@ module.exports = (req, res) => {
 
   console.log(notification);
   const url = new URL('https://api.notifymyecho.com/v1/NotifyMe');
-  url.searchParams.append('notification', notification);
+  url.searchParams.append('notification', Hay alguien en la puerta, notification);
   url.searchParams.append('accessCode', accessCode);
 
   https.get(url, ({ statusCode }) => {
