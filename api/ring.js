6a72dc58322f6f389380da211abf6a61873bd9f9
Chelo <marcelo.tissoni@gmail.com>
2024-02-24 10:45:03 -0300

    Update ring.js

--- api/ring.js
+++ api/ring.js
@@ -7,25 +7,59 @@ module.exports = (req, res) => {
     return;
   }
 
-  const notificatio = "Hello World!"; // Changed notification text
-  const accessCode = process.env.ALEXA_ACCESS_CODE;
+  const notificationText = "tocaron el timbre";
+  const accessToken = process.env.ALEXA_ACCESS_TOKEN; // Replace with your access token
 
-  if (!accessCode) {
-    throw new Error('Did you forget to assign ALEXA_ACCESS_CODE?');
+  if (!accessToken) {
+    throw new Error('Did you forget to assign ALEXA_ACCESS_TOKEN?');
   }
 
-  console.log(notification);
-  const url = new URL('https://api.notifymyecho.com/v1/NotifyMe');
-  url.searchParams.append('notification', notification);
-  url.searchParams.append('accessCode', accessCode);
-
-  https.get(url, ({ statusCode }) => {
-    const location = [200, 201, 202].includes(statusCode)
-      ? '/success.html'
-      : '/failure.html';
-    res.statusCode = 303;
-    res.setHeader('Location', location);
-    res.setHeader('Content-Type', 'text/html');
-    res.end(`Redirecting to <a href="${location}">${location}</a>...`);
+  const postData = JSON.stringify({
+    "spokenInfo": {
+      "content": [{
+        "locale": "es-ES",
+        "text": notificationText
+      }]
+    }
   });
+
+  const options = {
+    hostname: 'api.eu.amazonalexa.com',
+    port: 443,
+    path: '/v1/notifications',
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${accessToken}`,
+      'Content-Type': 'application/json',
+      'Content-Length': postData.length
+    }
+  };
+
+  const reqAPI = https.request(options, (response) => {
+    console.log(`statusCode: ${response.statusCode}`);
+
+    let responseData = '';
+
+    response.on('data', (chunk) => {
+      responseData += chunk;
+    });
+
+    response.on('end', () => {
+      console.log(JSON.parse(responseData));
+      const location = response.statusCode === 200 ? '/success.html' : '/failure.html';
+      res.statusCode = 303;
+      res.setHeader('Location', location);
+      res.setHeader('Content-Type', 'text/html');
+      res.end(`Redirecting to <a href="${location}">${location}</a>...`);
+    });
+  });
+
+  reqAPI.on('error', (error) => {
+    console.error(error);
+    res.statusCode = 500;
+    res.end('Internal Server Error');
+  });
+
+  reqAPI.write(postData);
+  reqAPI.end();
 };
